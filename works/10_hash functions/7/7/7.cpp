b06838ea8202802b539c69af8469567b96dfb601
4Raus <de.weiter.cttp@gmail.com>
2024-09-28 13:44:55 +0500

    added 3 more works

---
+++ works/10_hash functions/7/7/7.cpp
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+#define HASH_TABLE_SIZE 512
+#define N 2000
+#define BUFFER_SIZE 256
+
+using namespace std;
+
+typedef struct _Person {
+    char Surname[BUFFER_SIZE];
+} Person;
+
+int Exists(Person* personArr, Person* person) {
+    for (int i = 0; i < N; i++) {
+        if (strcmp(personArr[i].Surname, "none") != 0) {
+            if (strcmp(personArr[i].Surname, person->Surname) == 0) return 1;
+        }
+    }
+    return 0;
+}
+
+int readPersons(Person* personArr) {
+    FILE* inputFile = fopen("l7.txt", "r");
+    if (!inputFile) return 0;
+
+    char buff[BUFFER_SIZE];
+    int i = 0;
+    while (i < N && fscanf(inputFile, "%s", buff) == 1) {
+        Person temp;
+        strcpy(temp.Surname, buff);
+        if (Exists(personArr, &temp)) sprintf(personArr[i].Surname, "%s%d", buff, i);
+        else strcpy(personArr[i].Surname, buff);
+        i++;
+    }
+    fclose(inputFile);
+    return 1;
+}
+
+uint32_t GetHash(const Person* keyPerson) {
+    uint32_t hash = 0;
+    for (int i = 0; i < strlen(keyPerson->Surname); i++) hash = (hash << 2) - hash + keyPerson->Surname[i];
+    return hash % HASH_TABLE_SIZE;
+}
+
+void PrintPersonSurname(Person* personArr) {
+    for (int i = 0; i < N; i++) printf("0x%03x %s\n", GetHash(&personArr[i]), personArr[i].Surname);
+}
+
+int main() {
+    Person personArr[N];
+    for (int i = 0; i < N; strcpy(personArr[i].Surname, "none"), i++);
+
+    if (!readPersons(personArr)) printf("[ERR] Open a file");
+
+    int personHashMap[HASH_TABLE_SIZE] = { 0 };
+    for (int i = 0; i < N; i++) personHashMap[GetHash(&personArr[i])]++;
+    for (int i = 0; i < HASH_TABLE_SIZE; i++) printf("%d %d\n", i + 1, personHashMap[i]);
+
+    double k = HASH_TABLE_SIZE / (double)N;
+    double k_ = N / (double)HASH_TABLE_SIZE;
+
+    double stats = 0;
+    for (int i = 0; i < HASH_TABLE_SIZE; i++) stats += (personHashMap[i] - k_) * (personHashMap[i] - k_);
+
+    printf("M=%d N=%d\n", HASH_TABLE_SIZE, N);
+    printf("%lf [%lf %lf]", k * stats, HASH_TABLE_SIZE - sqrt(HASH_TABLE_SIZE), HASH_TABLE_SIZE + sqrt(HASH_TABLE_SIZE));
+
+    return 0;
+}
---
+++ works/9_AVL_tree_insert/6/6/6.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+};
+
+int getHeight(Node* node) {
+    if (node == nullptr) return 0;
+    return node->height;
+}
+
+int max(int a, int b) { return (a > b) ? a : b; }
+
+Node* createNode(int key) {
+    Node* newNode = new Node();
+    newNode->key = key;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    newNode->height = 1;
+    return newNode;
+}
+
+Node* rotateRight(Node* y) {
+    if (y == nullptr || y->left == nullptr) return y;
+    Node* x = y->left;
+    Node* z = x->right;
+
+    x->right = y;
+    y->left = z;
+
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    return x;
+}
+
+Node* rotateLeft(Node* x) {
+    if (x == nullptr || x->right == nullptr) return x;
+    Node* y = x->right;
+    Node* z = y->left;
+
+    y->left = x;
+    x->right = z;
+
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    return y;
+}
+
+int getBalance(Node* node) {
+    if (node == nullptr)  return 0;
+    return getHeight(node->left) - getHeight(node->right);
+}
+
+Node* rebalance(Node* tree, int key) {
+    int balance = getBalance(tree);
+
+    if (balance > 1 && key < tree->left->key) { return rotateRight(tree); }
+    else if (balance < -1 && key > tree->right->key) { return rotateLeft(tree); }
+    else if (balance > 1 && key > tree->left->key) {
+        tree->left = rotateLeft(tree->left);
+        return rotateRight(tree);
+    }
+    else if (balance < -1 && key < tree->right->key) {
+        tree->right = rotateRight(tree->right);
+        return rotateLeft(tree);
+    }
+    return tree;
+}
+
+Node* insert(Node* root, int key) {
+    if (root == nullptr) return createNode(key);
+
+    if (key < root->key) root->left = insert(root->left, key);
+    else root->right = insert(root->right, key);
+
+    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+    return rebalance(root, key);
+}
+
+void printTree(Node* root, string prefix = "", bool isLeft = true) {
+    if (root == nullptr) {
+        cout << prefix;
+        cout << (isLeft ? "L " : "   ") << "_\n";
+        return;
+    }
+
+    if (root->right != nullptr) printTree(root->right, prefix + (isLeft ? "|  " : "   "), false);
+
+    cout << prefix;
+    cout << (isLeft ? "L " : (root->left != nullptr ? "|-" : "   ")) << root->key << "\n";
+
+    if (root->left != nullptr) printTree(root->left, prefix + (isLeft ? "   " : "|  "), true);
+}
+
+int main() {
+    Node* root = nullptr;
+
+    int size;
+    cout << "Enter the number of elements: ";
+    cin >> size;
+
+    for (int i = 0; i < size; i++) {
+        int num;
+        cin >> num;
+        root = insert(root, num);
+    }
+
+    cout << "Tree structure:\n";
+    printTree(root);
+
+    return 0;
+}
